8bf9b2f5c516a805fb1cb3543cc762cb0ceb5be7
Nick Lee <Nicholas_Lee@uml.edu>
2019-07-08 16:56:42 -0400

    running multiple selenium test sequentially on crossbrowsertesting

--- src/uitest/index.js
+++ src/uitest/index.js
@@ -1,23 +1,110 @@
-import webdriver from 'selenium-webdriver';
-import { SeleniumServer } from 'selenium-webdriver/remote';
-import request from 'request';
+// import webdriver from 'selenium-webdriver';
+// import { SeleniumServer } from 'selenium-webdriver/remote';
+// import request from 'request';
 
-import { myUMLPopup_Selenium, solutionCenterWebsite_Selenium} from './selenium/index.js'
-
-export default class UITest {
-    constructor(url) {
-        this.remoteHub = "http://hub.crossbrowsertesting.com:80/wd/hub'";
-    }
-
-    start() {
-
-        const tasks = [myUMLPopup_Selenium, solutionCenterWebsite_Selenium];
-
-        tasks.reduce((results, currentFunc)=>{
-            await currentFunc()
-        }, )
+// import { myUMLPopup_Selenium, solutionCenterWebsite_Selenium} from './selenium/index.js'
 
 
-    }
+const webdriver = require('selenium-webdriver');
+const { SeleniumServer } = require('selenium-webdriver/remote')
+const request = require('request')
+
+const selenium = require('./selenium/index.js')
+
+
+//const {By, Key, until } = require("selenium-webdriver")
+
+
+// export default class UITest {
+//     constructor(url) {
+//         this.remoteHub = "http://hub.crossbrowsertesting.com:80/wd/hub'";
+//     }
+
+//     // start() {
+
+//     //     const tasks = [myUMLPopup_Selenium, solutionCenterWebsite_Selenium];
+
+//     //     tasks.reduce((results, currentFunc)=>{
+//     //         await currentFunc()
+//     //     }, )
+
+
+//     // }
+// }
+
+var scripts = [selenium.myUMLPopupTest_Selenium, selenium.solutionCenterWebsiteTest_Selenium]
+
+
+function methodThatReturnsAPromise(seleniumFunction,driver) {
+    return new Promise(async (resolve,reject)=>{
+        let value = await seleniumFunction(driver);
+        console.log('method that returns value: ', value)
+        resolve(value)
+  })
+}
+  
+function processScripts(){
+    
+
+    scripts.reduce( async (accum, func) =>{
+        await accum;
+
+        const caps = {
+            name : 'July 8',   // Name for Crossbrowser test
+            build :  '1.0',
+            version : '75x64', 
+            platform : 'Mac OSX 10.14', 
+            screen_resolution : '1366x768',
+            record_video : 'true',
+            record_network : 'false',
+            browserName : 'Chrome',
+            username : "",
+            password : ""
+        };
+    
+        const driver = new webdriver.Builder()
+            .usingServer("http://hub.crossbrowsertesting.com:80/wd/hub")
+            .withCapabilities(caps)
+            .build()
+            driver.manage().window().setRect({width: 1200, height: 600})
+
+        return methodThatReturnsAPromise(func,driver)
+    }, Promise.resolve())
+
 }
 
+  processScripts()
+
+
+webdriver.WebDriver.prototype.takeSnapshot = function() {
+
+    return new Promise((resolve, fulfill)=> { 
+        var result = { error: false, message: null }
+        
+        if (sessionId){
+            request.post(
+                'https://crossbrowsertesting.com/api/v3/selenium/' + sessionId + '/snapshots', 
+                function(error, response, body) {
+                    if (error) {
+                        result.error = true;
+                        result.message = error;
+                    }
+                    else if (response.statusCode !== 200){
+                        result.error = true;
+                        result.message = body;
+                    }
+                    else{
+                        result.error = false;
+                        result.message = 'success';
+                    }
+                }
+            )
+            .auth(username,authkey);   
+        }
+        else{
+            result.error = true;
+            result.message = 'Session Id was not defined';
+        }
+        result.error ? fulfill('Fail') : resolve('Pass'); //never call reject as we don't need this to actually stop the test
+    });
+}
--- src/uitest/selenium/index.js
+++ src/uitest/selenium/index.js
@@ -1,30 +1,36 @@
-import { By, Key, until } from "selenium-webdriver"
+// import { By, Key, until } from "selenium-webdriver"
 
+const {By, Key, until } = require("selenium-webdriver")
+const fs = require('fs')
 
-export async function myUMLPopupTest_Selenium(driver){
+ async function myUMLPopupTest_Selenium(driver){
     return new Promise(async (resolve,reject) => {
         try {
             await driver.get("https://stage.uml.edu/Student-Life/")
         
             const button = await driver.findElement(By.xpath('//*[@id="form"]/header/div/div[2]/nav/ul/li[4]'))
+            console.log('button:', button)
             await button.click();
-            //take picture here
             await driver.takeScreenshot().then( async (image, err) => {
-                const result = await writeFile('myUMLPopup.png', image, 'base64');            
-                console.log("File was saved!")
+                const result = await fs.writeFile('!myUMLPopup.png', image, 'base64', function(err){
+                    if(err){ throw err}
+                } );            
             });
-            const link = await driver.findElement(By.xpath('//*[@id="form"]/header/div/div[2]/nav/ul/li[4]/uml-myuml-popout/div/div/div/div/div/div[2]/div/div/ul/li[1]/a/span'))
+            const link = await driver.findElement(By.xpath('//*[@id="form"]/div[3]/div[4]/div/div/div/h1'))
             const text = await link.getText()
             //potentially want to click on links to navigate to new page
             resolve(text)
         } catch(error){
             reject(error)
+        } finally {
+            //await driver.showSnapshots()
+            driver.quit()
         }
     })
 }
 
 
-export async function solutionCenterWebsiteTest_Selenium(driver){
+ async function solutionCenterWebsiteTest_Selenium(driver){
     //searching 'bill' display articles
     return new Promise( async (resolve, reject)=>{
 
@@ -39,12 +45,23 @@ export async function solutionCenterWebsiteTest_Selenium(driver){
             //this could probably be one function 
             const firstResult = await driver.findElement(By.xpath('/html/body/div[1]/uml-app-knowledge-base/div[2]/div/div/div/div/div[1]/div/div/div[3]/div[1]/div[1]/a/span'))
             const text = await firstResult.getText()
+            console.log("from selenium func", text)
 
             resolve(text)
         } catch(error){
             console.error(error)
             reject(error)
+        } finally {
+            //await driver.showSnapshots()
+            driver.quit()
         }
 
     })
+}
+
+
+
+module.exports = {
+    myUMLPopupTest_Selenium:myUMLPopupTest_Selenium,
+    solutionCenterWebsiteTest_Selenium: solutionCenterWebsiteTest_Selenium
 }
