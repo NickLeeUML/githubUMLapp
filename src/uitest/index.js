b80914123db5e9f68973bcdee825c7fdd0bcbc27
Nick Lee <Nicholas_Lee@uml.edu>
2019-07-23 12:43:09 -0400

    first working example

--- src/uitest/index.js
+++ src/uitest/index.js
@@ -6,6 +6,7 @@ import request from 'request';
 import { myUMLPopupTest_Selenium, solutionCenterWebsiteTest_Selenium } from './selenium/index.js';
 
 import dotenv from 'dotenv';
+import { promises } from 'fs';
 dotenv.config();
 
 const selenium_scripts = [myUMLPopupTest_Selenium];
@@ -20,42 +21,42 @@ export default class UITest {
         };
         this.remoteHub = "http://hub.crossbrowsertesting.com:80/wd/hub'";
         this.browserConfigurations = {
-            windows10Chrome: {
-                browserName: 'Chrome',
-                version: '75x64',
-                platform: 'Windows 10',
-                screenResolution: '1366x768',
-            },
+            // windows10Chrome: {
+            //     browserName: 'Chrome',
+            //     version: '75x64',
+            //     platform: 'Windows 10',
+            //     screenResolution: '1366x768',
+            // },
             windows10Firefox: {
                 browserName: 'Firefox',
                 version: '67x64',
                 platform: 'Windows 10',
                 screenResolution: '1366x768',
             },
-            windows10Edge: {  // erroring out 
-                browserName: 'MicrosoftEdge',
-                version: '18',
-                platform: 'Windows 10',
-                screenResolution: '1366x768',
-            },
+            // windows10Edge: {  // erroring out 
+            //     browserName: 'MicrosoftEdge',
+            //     version: '18',
+            //     platform: 'Windows 10',
+            //     screenResolution: '1366x768',
+            // },
             osxSafari: {
                 browserName: 'Safari',
                 version: '12',
                 platform: 'Mac OSX 10.14',
                 screenResolution: '1366x768',
             },
-            osxFirefox: {
-                browserName: 'Firefox',
-                version: '67',
-                platform: 'Mac OSX 10.14',
-                screenResolution: '1366x768',
-            },
-            osxChrome: {
-                browserName: 'Chrome',
-                version: '75x64',
-                platform: 'Mac OSX 10.14',
-                screenResolution: '1366x768',
-            },
+            // osxFirefox: {
+            //     browserName: 'Firefox',
+            //     version: '67',
+            //     platform: 'Mac OSX 10.14',
+            //     screenResolution: '1366x768',
+            // },
+            // osxChrome: {
+            //     browserName: 'Chrome',
+            //     version: '75x64',
+            //     platform: 'Mac OSX 10.14',
+            //     screenResolution: '1366x768',
+            // },
         };
         this.screenSizes = {
             small: {
@@ -76,20 +77,32 @@ export default class UITest {
     }
 
     start() {
-        this.status.running = true;
-        const browserConfigurationsArray = Object.values(this.browserConfigurations);
 
-        let results = browserConfigurationsArray.reduce(async (accum, current) => {
-            //current is data for pass to functio
-            await accum;
+        return new Promise((resolve, reject) => {
 
-            return processScripts(current, this.hash);
-        }, Promise.resolve());
+            this.status.running = true;
+            const browserConfigurationsArray = Object.values(this.browserConfigurations);
+    
+            let results = browserConfigurationsArray.reduce(async (accum, current) => {
+                //current is data for pass to functio
+                await accum;
+    
+                return processScripts(current, this.hash);
+            }, Promise.resolve());
+    
+            results.then((e) => {
+                console.log('all done');
+                this.status.running = false;
+                resolve("success")
+            }).catch(e => {
+                console.log("error broski")
+                reject(e)
+            });
 
-        results.then((e) => {
-            console.log('all done');
-            this.status.running = false;
-        });
+        })
+
+    
+      
     }
 
     changeStatus = (running, error, message) => {
@@ -155,5 +168,5 @@ function processScripts(capability, hash) {
     });
 }
 
-const test = new UITest( );
-test.start();
+// const test = new UITest( );
+// test.start();
